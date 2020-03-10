7e5f9c39c032a94ebe84f35389a3d1d8ca2a1551
Lee Norris <lee.norris@arcadia.io>
2020-03-10 13:11:02 -0400

    chore: not working

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-import { AppModule } from './src/app/app.module';
+import { AppModule } from './srcOg/app/app.module';
 import { NestFactory } from '@nestjs/core';
 import { Subject } from 'rxjs';
 
 const inboundStream = new Subject();
 const outboundStream = new Subject();
 async function bootstrap() {
-    await NestFactory.createApplicationContext(AppModule.forRoot(inboundStream, outboundStream), { logger: console });
+    await NestFactory.createApplicationContext(AppModule.forRoot(inboundStream.asObservable(), outboundStream), { logger: console });
     outboundStream.subscribe(d => console.log(d))
     inboundStream.next('hi');
 }
--- src/libs/routing/i-listener.interface.ts
+++
@@ -1,7 +0,0 @@
-import { AppEvents } from "./app-events.enum";
-import { Subject } from "rxjs";
-
-export interface IListener {
-    event: AppEvents;
-    respond: (eventData: any, outbound: Subject<any>) => void;
-}
