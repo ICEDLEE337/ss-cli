d463e8be50cfd413d3b7085830e26ce6d7a50fe8
Lee Norris <lee.norris@arcadia.io>
2020-03-10 13:30:25 -0400

    chore: worx better!

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { AppModule, Me } from './app.module';
 import { AppService } from './app.service';
 import { Subject } from 'rxjs';
 
+const one = new Subject();
+const two = new Subject();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule.forRoot(new Subject().asObservable(), new Subject()));
+  const app = await NestFactory.create(AppModule.forRoot(one.asObservable(), two));
   const thing = await app.get(Me);
+
+one.next('hi');
+two.subscribe(d => console.log(new Date().toISOString() + d));
+
   console.log(thing);
   // await app.listen(3000);
 }
