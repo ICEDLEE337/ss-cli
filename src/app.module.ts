251ced640b0ff83c3dbbd75c26f6df599dddbd20
Lee Norris <lee.norris@arcadia.io>
2020-03-10 14:12:32 -0400

    chore: cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,12 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
-
-import { Module, DynamicModule, Injectable } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { RoutingModule } from './routing/routing.module';
-import { Subject, Observable } from 'rxjs';
-
-@Injectable()
-export class Me {
-    name = 'lee';
-    age = 39;
-}
 
 @Module({})
 export class AppModule {
-    static forRoot (inboundStream: Observable<any>, outboundStream: Subject<any>): DynamicModule {
+    static forRoot (inboundStream: any, outboundStream: any): DynamicModule {
         return {
             module: AppModule,
-            imports: [RoutingModule.forRoot(inboundStream, outboundStream)],
-            providers: [{provide: Me, useClass: Me}],
-            exports: [Me]
+            imports: [RoutingModule.forRoot(inboundStream, outboundStream)]
         };
     }
 }
