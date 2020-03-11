8b08a27f8f8c2fa69f1ada31c4626ea260c51bd2
Lee Norris <lee.norris@arcadia.io>
2020-03-10 20:42:37 -0400

    chore: cleanup

--- src/core/arg.decorator.ts
+++ src/core/arg.decorator.ts
@@ -3,18 +3,12 @@ import * as minimist from 'minimist';
 import * as kebab from 'lodash.kebabcase';
 export const Arg = (ctrFn: Function) =>
     function argDecorator(constructor: any) {
-        console.log(arguments);
-        console.log(constructor.name, ctrFn);
         const args = minimist(process.argv.slice(2), { default: {} });
         const key = kebab(constructor.name).replace('-parameter', '').toLowerCase();
-        console.log(key);
-        console.log(args);
-
-        const typedValue = ctrFn(args[key]);
 
         return class extends constructor {
             value = args[key];
-            typedValue = typedValue;
+            typedValue = ctrFn(args[key]);
             name = constructor.name;
         };
     };
